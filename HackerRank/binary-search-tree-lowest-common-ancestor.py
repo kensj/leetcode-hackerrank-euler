19425c8d948021e38cff989abf1466e6420609aa
kensj <kenji.japra@gmail.com>
2018-09-21 21:10:25 -0400

    update

--- HackerRank/binary-search-tree-lowest-common-ancestor.py
+++ HackerRank/binary-search-tree-lowest-common-ancestor.py
@@ -1,13 +1,6 @@
 def lca(root, v1, v2):
-    found = valExists(root, v1) or valExists(root, v2)
-    return root if found else lca(root.left, v1, v2) or lca(root.right, v1, v2)
-
-def valExists(root, val):
-    if not root:
-        return False
-    if root.info == val:
-        return True
-    left = valExists(root.left, val)
-    right = valExists(root.right, val)
-    return left or right
-	
+    if root.info < v1 and root.info < v2:
+        return lca(root.right, v1, v2)
+    if root.info > v2 and root.info > v1:
+        return lca(root.left, v1, v2)
+    return root
